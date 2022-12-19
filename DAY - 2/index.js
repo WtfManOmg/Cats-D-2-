33e3c1f2b63c9e70fc62ea89ceda11c574328cdc
WtfManOmg <113549616+WtfManOmg@users.noreply.github.com>
2022-12-19 12:05:29 +0300

    test

---
+++ DAY - 2/index.js
@@ -0,0 +1,30 @@
+console.log("work");
+
+const $wr = document.querySelector('[data-wr]')
+
+
+const getCatHTML = (cat) => {
+	return `
+		<div class="card mb-2" style="width: 18rem">
+		<img src="${cat.image}" class="card-img-top" alt="${cat.name}" />
+		<div class="card-body">
+			<h5 class="card-title">${cat.name}</h5>
+			<p class="card-text">
+				${cat.description}
+			</p>
+			<a href="#" class="btn btn-primary">Подробнее</a>
+		</div>
+	</div>
+	`
+}
+
+fetch('https://cats.petiteweb.dev/api/single/WtfManOmg/show/')
+	.then((res) => res.json())
+	.then((data) => {
+	
+		$wr.insertAdjacentHTML('afterbegin', data.map(cat => getCatHTML(cat)).join('') )
+		
+		console.log({data})
+	
+	
+	})
